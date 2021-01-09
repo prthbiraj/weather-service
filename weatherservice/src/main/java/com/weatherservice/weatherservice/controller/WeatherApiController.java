a62e4a737adfd8c169f85c5e1f31ddb6c06c5191
Prabhat Thakur <prthbiraj@gmail.com>
2021-01-09 13:21:24 +0530

    update weather service

---
+++ weatherservice/src/main/java/com/weatherservice/weatherservice/controller/WeatherApiController.java
@@ -0,0 +1,31 @@
+package com.weatherservice.weatherservice.controller;
+
+import com.weatherservice.weatherservice.model.Weather;
+import com.weatherservice.weatherservice.model.WeatherForecast;
+import com.weatherservice.weatherservice.services.WeatherService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherApiController {
+
+    private final WeatherService weatherService;
+
+    public WeatherApiController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @RequestMapping("/now/{country}/{city}")
+    public Weather getWeather(@PathVariable String country,
+                              @PathVariable String city) {
+        return this.weatherService.getWeather(country, city);
+    }
+
+    @RequestMapping("/weekly/{country}/{city}")
+    public WeatherForecast getWeatherForecast(@PathVariable String country,
+                                              @PathVariable String city) {
+        return this.weatherService.getWeatherForecast(country, city);
+    }
+}
